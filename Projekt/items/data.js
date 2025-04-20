03d8fc66d2bcb56a910f6408bb5be6a384226adc
fckdsn <dvrk212121@gmail.com>
2025-04-20 21:00:17 +0200

    items added

---
+++ Projekt/items/data.js
@@ -0,0 +1,139 @@
+// Пример одного товара
+const products = [
+  {
+    id: 1,
+    title: "Nike",
+    pricePln: 360,
+    brand: "True Religion",
+    size: "US 10",
+    color: "Black",
+    condition: "New",
+    returnPolicy: "14 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 2,
+    title: "Adidas Forum Low",
+    pricePln: 319,
+    brand: "Adidas",
+    size: "US 9",
+    color: "White",
+    condition: "New",
+    returnPolicy: "30 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 3,
+    title: "New Balance 530",
+    pricePln: 410,
+    brand: "New Balance",
+    size: "US 11",
+    color: "Grey",
+    condition: "Like New",
+    returnPolicy: "14 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 4,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 5,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 6,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 7,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 8,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 9,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 10,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 11,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  },
+  {
+    id: 12,
+    title: "Puma RS-X",
+    pricePln: 295,
+    brand: "Puma",
+    size: "US 8",
+    color: "Red",
+    condition: "Used",
+    returnPolicy: "7 Day Return",
+    images: ["../images/placeholder.jpg"]
+  }
+
+];
+
+
+
